c755b9545ce80b07af3d2d14110f5de76fd516ac
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-07-22 22:41:31 -0600

    creating addEvent page, eventDetails page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
         <View style={constantStyles.footer}>
             <TouchableOpacity onPress={() => router.push(`/`)}><Feather name='home' style={constantStyles.tabButton}></Feather></TouchableOpacity>
             <TouchableOpacity onPress={() => router.push(`../calendarPage`)}><Feather name='calendar' style={constantStyles.tabButton}></Feather></TouchableOpacity>
-            <TouchableOpacity onPress={() => router.push(`/`)}><Feather name='plus-square' style={constantStyles.tabButton}></Feather></TouchableOpacity>
+            <TouchableOpacity onPress={() => router.push(`../addEvent`)}><Feather name='plus-square' style={constantStyles.tabButton}></Feather></TouchableOpacity>
             <TouchableOpacity onPress={() => router.push(`../profile`)}><Feather name='user' style={constantStyles.tabButton}></Feather></TouchableOpacity>
         </View>
     );
