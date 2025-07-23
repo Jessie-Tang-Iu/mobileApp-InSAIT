639a6301cbee9ab2179841a3847ad0880d51520c
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-07-22 20:40:53 -0600

    Creating calendar page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     return (
         <View style={constantStyles.footer}>
             <TouchableOpacity onPress={() => router.push(`/`)}><Feather name='home' style={constantStyles.tabButton}></Feather></TouchableOpacity>
-            <TouchableOpacity onPress={() => router.push(`/`)}><Feather name='calendar' style={constantStyles.tabButton}></Feather></TouchableOpacity>
+            <TouchableOpacity onPress={() => router.push(`../calendarPage`)}><Feather name='calendar' style={constantStyles.tabButton}></Feather></TouchableOpacity>
             <TouchableOpacity onPress={() => router.push(`/`)}><Feather name='plus-square' style={constantStyles.tabButton}></Feather></TouchableOpacity>
             <TouchableOpacity onPress={() => router.push(`../profile`)}><Feather name='user' style={constantStyles.tabButton}></Feather></TouchableOpacity>
         </View>
